00a9fe724f83934325f552b727ec313449344eaf
Lucas Costa <lucaseduardobcc@gmail.com>
2021-07-28 00:06:13 -0300

    first commit

---
+++ src/main/java/lucaseduardoferreira/com/github/dockerteste/DockertesteApplication.java
@@ -0,0 +1,13 @@
+package lucaseduardoferreira.com.github.dockerteste;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockertesteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DockertesteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/lucaseduardoferreira/com/github/dockerteste/DockertesteApplicationTests.java
@@ -0,0 +1,13 @@
+package lucaseduardoferreira.com.github.dockerteste;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DockertesteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
